8db59da8505220537025abfd4c92bb5dcb12aba5
andrijakrklec <andrija.krklec@fer.hr>
2023-11-05 21:49:29 +0100

    popravljena normalizacija baze

--- naredbe_export.sql
+++ naredbe_export.sql
@@ -1,40 +1,69 @@
 COPY (
-    SELECT array_to_json(array_agg(row_to_json(t))) FROM (
-        SELECT 
-            borci.ime,
-			borci.prezime,
-			rekord,
-			datum_rođenja,
-			preciznost_značajnih_udaraca,
-		    broj_značajnih_udaraca_po_minuti,
-			preciznost_rušenja,
-			broj_rušenja_po_minuti,
-
-            COALESCE(json_agg(
-                        json_build_object(
-                            'protivnik', protivnik_ime || ' ' || protivnik_prezime, 
-                            'rezultat', rezultat, 'datum_borbe', datum_borbe
-						)	
-					), '[]'
-			) AS prethodne_borbe
-
-        FROM borci join borbe on (
-			borci.ime = borbe.ime and
-			borci.prezime = borbe.prezime and
-			datum_borbe = datum_prethodne_borbe)
-		GROUP BY borci.ime, borci.prezime, rekord, datum_rođenja, 
-			preciznost_značajnih_udaraca,
-		    broj_značajnih_udaraca_po_minuti,
-			preciznost_rušenja,
-			broj_rušenja_po_minuti
-        ) t ) 
-
-TO 'D://UFC_borci.json';
-
+  SELECT 
+    array_to_json(
+      array_agg(
+        row_to_json(t)
+      )
+    ) 
+  FROM 
+    (
+      SELECT 
+        borci.id,
+		borci.ime, 
+        borci.prezime, 
+        borci.rekord, 
+        borci.datum_rođenja, 
+        borci.preciznost_značajnih_udaraca, 
+        borci.broj_značajnih_udaraca_po_minuti, 
+        borci.preciznost_rušenja, 
+        borci.broj_rušenja_po_minuti, 
+        COALESCE(
+          json_agg(
+            json_build_object(
+              'protivnik', b.ime || ' ' || b.prezime, 
+              'rezultat', rezultat, 'datum_borbe', 
+              datum_borbe
+            )
+          ), 
+          '[]'
+        ) AS prethodne_borbe 
+      FROM 
+        borci 
+        join borbe on (
+          borci.id = borbe.id 
+        ) 
+        join borci b on (b.id = borbe.protivnik_id) 
+      GROUP BY 
+        borci.id, 
+        borci.ime, 
+        borci.prezime, 
+        borci.rekord, 
+        borci.datum_rođenja, 
+        borci.preciznost_značajnih_udaraca, 
+        borci.broj_značajnih_udaraca_po_minuti, 
+        borci.preciznost_rušenja, 
+        borci.broj_rušenja_po_minuti
+    ) t
+) TO 'D://podaci.txt';
 COPY (
-    SELECT borci.*, protivnik_ime,protivnik_prezime, rezultat FROM borci JOIN borbe ON (
-	borci.ime = borbe.ime AND
-	borci.prezime = borbe.prezime AND
-	datum_borbe = datum_prethodne_borbe)
-) 
-TO 'D://UFC_borci.csv' WITH DELIMITER ',' CSV HEADER;
+  SELECT 
+    borci.id, 
+    borci.ime, 
+    borci.prezime, 
+    borci.rekord, 
+    borci.datum_rođenja, 
+    borci.preciznost_značajnih_udaraca, 
+    borci.broj_značajnih_udaraca_po_minuti, 
+    borci.preciznost_rušenja, 
+    borci.broj_rušenja_po_minuti, 
+    b.ime protivnik_ime, 
+    b.prezime protivnik_prezime, 
+	borbe.datum_borbe,
+    rezultat 
+  from 
+    borci 
+    join borbe on (
+      borci.id = borbe.id 
+    ) 
+    join borci b on (b.id = borbe.protivnik_id)
+) TO 'D://podaci.csv' WITH DELIMITER ',' CSV HEADER;
